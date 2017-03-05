b57dc0015d87482ac6e7bfe6abe3e786ded997a4
vlad101 <V_efros85@hotmail.com>
2017-03-05 11:28:28 -0500

    completed setting buttons

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,32 @@
+package com.bignerdranch.android.geoquiz;
+
+/**
+ * Created by vladimir on 3/5/17.
+ */
+
+public class Question {
+
+    private int mTextResId;
+    private boolean mAnswerTrue;
+
+    public Question(int mTextResId, boolean mAnswerTrue) {
+        this.mTextResId = mTextResId;
+        this.mAnswerTrue = mAnswerTrue;
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -3,14 +3,25 @@ package com.bignerdranch.android.geoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Gravity;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+    private Question[] mQuestionBank = new Question [] {
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true),
+    };
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,13 +29,15 @@ public class QuizActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        // attach question text view
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
         // attach true button
         mTrueButton = (Button) findViewById(R.id.true_button);
         // set click listener
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
         // attach false button
@@ -33,8 +46,43 @@ public class QuizActivity extends AppCompatActivity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+        // attach next button
+        mNextButton = (Button) findViewById(R.id.next_button);
+        // set click event
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // increment current index
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                // set next question
+                updateQuestion();
+            }
+        });
+        // set first question
+        updateQuestion();
+    }
+
+    /**
+     * Update question.
+     */
+    private void updateQuestion() {
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+        mQuestionTextView.setText(question);
+    }
+
+    /**
+     * Check answer.
+     */
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+        int messageResId = 0;
+        if(userPressedTrue == answerIsTrue)
+            messageResId = R.string.correct_toast;
+        else
+            messageResId = R.string.incorrect_toast;
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
